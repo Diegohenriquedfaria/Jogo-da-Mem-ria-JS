6ff7942be33aa6c079fc9f863ea9b4d04924a5ca
Diego Henrique De Faria <diegohenrique.defr@gmail.com>
2025-10-15 15:38:34 -0300

    atualizando novo repositorio

---
+++ OneDrive/Área de Trabalho/CTRLPLAY/PROGRAMAÇAODIEGO/Projetos-diego.web/Jogo Memoria JS/jogo_memoria/src/script/main.js
@@ -0,0 +1,19 @@
+let menu = document.getElementById('menu');
+let select = document.getElementById('numCards');
+let start = document.getElementById('start');
+
+for (let i = 4; i <= 10; i+=2){
+
+    let quantiaCartas  = i*i;
+    let opção = document.createElement('option');
+
+    opção.value = quantiaCartas;
+    opção.innerHTML =  quantiaCartas;
+
+    select.appendChild(opção);
+
+
+    
+
+
+}
